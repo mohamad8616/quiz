5f11153fbbf49dfb62ffa5a2e4f5a3943a3c95fb
mohamad8616 <mohamad.sh.a9036@gmail.com>
2025-02-27 19:26:34 +0330

    created project base

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,5 @@
+function appLayout() {
+  return <div></div>;
+}
+
+export default appLayout;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div className='h-3/12 bg-amber-50'>Header</div>;
+}
+
+export default Header;
